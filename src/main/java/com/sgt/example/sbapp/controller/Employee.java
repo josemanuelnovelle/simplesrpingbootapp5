23b4cc11ba5a5950a8179a515cb1fe576a49bb63
josemanuelnovelle <52532858+josemanuelnovelle@users.noreply.github.com>
2019-07-14 12:48:34 +0200

    Update Employee.java

--- src/main/java/com/sgt/example/sbapp/controller/Employee.java
+++ src/main/java/com/sgt/example/sbapp/controller/Employee.java
@@ -7,62 +7,30 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+ 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 
-	private long id;
-	private String firstName;
-	private String lastName;
-	private String emailId;
-	
-	public Employee() {
-		
-	}
-	
-	public Employee(String firstName, String lastName, String emailId) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.emailId = emailId;
-	}
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
+	private long id;
 	
-	@Column(name = "first_name", nullable = true)
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
 	
-	@Column(name = "last_name", nullable = true)
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+	@Column(name = "last_name", nullable = false)
+	private String lastName;
 	
-	@Column(name = "email_address", nullable = true)
-	public String getEmailId() {
-		return emailId;
-	}
-	public void setEmailId(String emailId) {
-		this.emailId = emailId;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
-				+ "]";
-	}
+	@Column(name = "email_address", nullable = false)
+	private String emailId;
+ 
 	
 }
